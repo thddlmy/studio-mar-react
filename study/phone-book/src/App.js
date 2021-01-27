ec1f70334ef4e37fe0686f1d4a470cd83abfa471
thddlmy <thddlmy@gmail.com>
2021-01-27 12:27:12 +0900

    2021-01-27 12:27 commit

--- study/phone-book/src/App.js
+++ study/phone-book/src/App.js
@@ -1,15 +1,66 @@
 import React, { Component } from 'react';
-import './App.css';
-import PhoneForm from './components/PhoneForm'
+import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ],
+    keyword: ''
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+  }
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id ? {...info, ... data } : info
+      )
+      // 만약 값이 다르면 새 객체를 만들어라!
+      // 같다면 기존 info 유지!
+    });
+  }
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    });
   }
   render() {
+    const { information, keyword } = this.state;
+    const filteredList = information.filter(
+      info => info.name.indexOf(keyword) !== -1
+    );
     return (
       <div>
-        <PhoneForm onCreate={this.handleCreate}/>
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
+        <p>
+          <input placeholder="검색 할 이름을 입력하세요" onChange={this.handleChange} value={keyword}/>
+        </p>
+        <hr />
+        <PhoneInfoList data={filteredList} onRemove={this.handleRemove} onUpdate={this.handleUpdate}/>
       </div>
     );
   }
